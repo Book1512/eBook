35bd6a14913b537e216db4c5c57016813caf832e
Prince <bookarhive1512@gmail.com>
2022-09-11 15:55:41 +0530

    Updates

---
+++ precache-manifest.a12a4296d3b53ce5bf4ae8753733c9c5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ac175114f8d75115124",
+    "url": "/eBook/static/css/main.824581e1.chunk.css"
+  },
+  {
+    "revision": "2ac175114f8d75115124",
+    "url": "/eBook/static/js/main.2ac17511.chunk.js"
+  },
+  {
+    "revision": "4e3eec0efd1bea118b1d",
+    "url": "/eBook/static/js/1.4e3eec0e.chunk.js"
+  },
+  {
+    "revision": "61a295ac30b9ff421482",
+    "url": "/eBook/static/css/2.a1ba243d.chunk.css"
+  },
+  {
+    "revision": "61a295ac30b9ff421482",
+    "url": "/eBook/static/js/2.61a295ac.chunk.js"
+  },
+  {
+    "revision": "6ef2f6f9c89cc9042cc6",
+    "url": "/eBook/static/js/runtime~main.6ef2f6f9.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/eBook/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/eBook/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/eBook/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/eBook/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "72d522d7fc302c1b96044fbcc279489a",
+    "url": "/eBook/index.html"
+  }
+];
