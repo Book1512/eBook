38b0843de6c1523e0139d67f900b46c647be1c33
Prince <bookarhive1512@gmail.com>
2022-09-11 16:31:38 +0530

    Final Update Sem-5 -- 2

--- src/component/footer.js
+++ src/component/footer.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { RiCopyrightLine } from "react-icons/ri";
 export default function Footer() {
   return (
-    <div className='footer'>
+    <div className='footer' id='footer'>
         <div className='foorter_content d-flex bg-secondary justify-content-center p-2' style={{color:'#fff'}}>
-            <div className=''>
+            <div className='' id='text'>
             Copyright &nbsp;<RiCopyrightLine/> 2022 , Book Archive &nbsp;
             </div>
-            <div className=''>All Right Reserved</div>
+            <div className='' id='text'>All Right Reserved</div>
         </div>
     </div>
   )
